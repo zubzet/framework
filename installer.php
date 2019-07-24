637d27056a6c39bbea370583449e4fa59f060318
LordVonAdel <adrian.urban@zierhut-it.de>
2019-07-24 16:27:31 +0200

    Reorganized core assets

--- installer.php
+++ installer.php
@@ -163,6 +163,7 @@ if (isset($_POST["db-host"])) {
         createDirectory("../assets");
         createDirectory("../assets/js");
         createDirectory("../assets/css");
+        createDirectory("../assets/css/font-awesome");
 
         //Create config file
         file_put_contents("../z_config/z_settings.ini", $configText);
@@ -170,10 +171,13 @@ if (isset($_POST["db-host"])) {
         //Creating entrance
         copy("install/index.php", "../index.php");
         copy("install/.htaccess", "../.htaccess");
-        copy("default/assets/Z.js", "../assets/js/Z.js");
+        copy("default/assets/js/Z.js", "../assets/js/Z.js");
+        copy("default/assets/js/jquery.min.js", "../assets/js/jquery.min.js");
+        copy("default/assets/js/bootstrap.min.js", "../assets/js/bootstrap.min.js");
         copy("default/assets/css/bootstrap.min.css", "../assets/css/bootstrap.min.css");
         copy("default/assets/css/loadCircle.css", "../assets/css/loadCircle.css");
         copy("default/assets/css/font-awesome.css", "../assets/css/font-awesome.css");
+        copy("default/assets/css/font-awesome/all.min.css", "../assets/css/font-awesome/all.min.css");
         copy("cv.txt", "../.z_framework");
 
         //Composer shit
