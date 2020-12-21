3dcf5dfba5f8a9c79bbda31bc7251d6ea2193358
Alexander Zierhut <info@alzlper.com>
2020-12-21 04:11:24 +0100

    Removed token expired message

--- layout_essentials.php
+++ layout_essentials.php
@@ -48,54 +48,15 @@ function essentialsHead($opt, $customBootstrap) { ?>
  * @param object $opt Object holding options for rendering
  */
 function essentialsBody($opt) { ?>
-    <!-- TOKEN EXPIRED MESSAGE -->
+    <!-- TOKEN EXPIRED -->
     <?php if($opt["user"]->isLoggedIn) { ?>
-    <style>
-        .tokenExpired {
-            position: fixed;
-            top: 0px;
-            left: 0px;
-            width: 100%;
-            height: 100%;
-            background: white;
-        }
-
-        .tokenExpired iframe {
-            border: none;
-        }
-    </style>
-
-    <div class="tokenExpired invisible" id="tokenExpired">
-        <h1 class="text-center"><?php $opt["lang"]("token_expired_heading"); ?></h1>
-        <p class="lead text-center"><?php $opt["lang"]("token_expired_explanation"); ?></p>
-        <iframe id="loginFrame" class="login-frame" width="100%"></iframe>
-    </div>
-    
-    <script>
-        var timedOut = false;
-
-        var iframe = document.getElementById("loginFrame");
-        iframe.onload = function() {
-            if (iframe.contentWindow.document.body) {
-                iframe.height = (iframe.contentWindow.document.body.scrollHeight + 10) + "px";
-            }
-        }
-
-        var token_expired_callback = setInterval(function() {
-            if (!timedOut) {
+        <script>
+            var token_expired_callback = setInterval(function() {
                 if(document.cookie.indexOf("z_login_token") < 0) {
-                    timedOut = true;
-                    $("#tokenExpired").removeClass("invisible");
-                    $("#loginFrame").attr("src", '<?php echo $opt["root"]; ?>login?noLayout=true');
+                    location.reload();
                 }
-            }
-            if (timedOut) {
-                if(document.cookie.indexOf("z_login_token") >= 0) {
-                    timedOut = false;
-                    $("#tokenExpired").addClass("invisible");
-                }
-            }
-        }, 1000);
-    </script>
-    <!-- TOKEN EXPIRED MESSAGE -->
-<?php } } ?>
+            }, 1000);
+        </script>
+        <!-- TOKEN EXPIRED -->
+    <?php } ?>
+<?php } ?>
