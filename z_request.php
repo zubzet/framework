6057bb9e8d8a5991a025ff27ac9605366807c8ba
ulrich.noah <noah.ulrich@zierhut-it.de>
2025-07-22 12:45:42 +0200

    ✨ Add functionality to get specific Parts of urlParameters
    Signed-off-by: ulrich.noah <noah.ulrich@zierhut-it.de>

--- z_request.php
+++ z_request.php
@@ -13,6 +13,14 @@
 
         public array $urlParameters = [];
 
+        public function getRouteParameter($key = null) {
+            if(isset($key)) {
+                return $this->urlParameters[$key] ?? null;
+            }
+
+            return $this->urlParameters;
+        }
+
         /**
          * @var array Store values within the Request to pass through data within internal redirects
          */
