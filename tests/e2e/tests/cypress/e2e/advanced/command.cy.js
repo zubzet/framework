4521a3e88951e16e9777f8d6c4322bed865befc4
zierhut.alex <alex@zierhut-it.de>
2025-09-08 14:19:52 +0200

    test(command): fix broken e2e tests
    Signed-off-by: zierhut.alex <alex@zierhut-it.de>

--- tests/e2e/tests/cypress/e2e/advanced/command.cy.js
+++ tests/e2e/tests/cypress/e2e/advanced/command.cy.js
@@ -4,7 +4,7 @@ describe('Commands', () => {
     });
 
     it('Execute Command', () => {
-        cy.exec('docker exec application php8 index.php run core index').then((result) => {
+        cy.exec('docker exec application php index.php run core index').then((result) => {
             const output = result.stdout;
 
             expect(output).to.include('Controller Index');
@@ -12,7 +12,7 @@ describe('Commands', () => {
     });
 
     it('Execute Command with Parameters', () => {
-        cy.exec('docker exec application php8 index.php run core parameters test und so').then((result) => {
+        cy.exec('docker exec application php index.php run core parameters test und so').then((result) => {
             const output = result.stdout;
 
             expect(output).to.include('Array\n(\n    [0] => test\n    [1] => und\n    [2] => so\n)');
