a78b1c4d0eaa8763b82790d0b944b51ef147f835
ulrich.noah <noah.ulrich@zierhut-it.de>
2025-09-09 11:03:43 +0200

    fix: set cookie before output to avoid header error
    Move `echo $userId` after `$res->loginAs(1)` in AdminController so Set-Cookie is sent first.
    
    Signed-off-by: ulrich.noah <noah.ulrich@zierhut-it.de>

--- tests/e2e/app/Controllers/AdminController.php
+++ tests/e2e/app/Controllers/AdminController.php
@@ -3,9 +3,10 @@
     class AdminController extends z_controller {
 
         public function action_loginas(Request $req, Response $res) {
-            echo($req->getRequestingUser()->userId ?? 0);
-
+            $userId = $req->getRequestingUser()->userId ?? 0;
             $res->loginAs(1);
+
+            echo($userId);
         }
 
     }
