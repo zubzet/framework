89f3461e86c76196b12ffe1977cda77d6d4d2d2c
zierhut.alex <alex@zierhut-it.de>
2026-04-05 23:29:53 +0200

    chore(refactor): Extract request functionality into separate traits
    Signed-off-by: zierhut.alex <alex@zierhut-it.de>

---
+++ src/Form/Validation/CanValidateForm.php
@@ -0,0 +1,124 @@
+<?php
+
+    namespace ZubZet\Framework\Form\Validation;
+
+    use ZubZet\Framework\Form\Validation\Field;
+    use ZubZet\Framework\Form\Validation\Result;
+
+    trait CanValidateForm {
+
+        /**
+         * Validates form data from the client
+         * @param Field[] $fields Array of fields with the validation rules
+         * @param array $data Input for the validation. $_GET or $_POST can be used here as parameters
+         * @return Result A result to work with
+         */
+        public function validateForm($fields, $data = null) {
+            $errors = [];
+
+            if ($data == null) {
+                $data = array_merge($_POST, $_FILES);
+            }
+
+            $formResult = new Result();
+            $formResult->fields = $fields;
+
+            foreach ($fields as $field) {
+                $name = $field->name;
+
+                $field->value = $data[$name] ?? null;
+
+                foreach ($field->rules as $rule) {
+                    $type = $rule["type"];
+
+                    if ($type == "required") {
+
+                        if (!((isset($data[$name]) && $data[$name] != "" ) || isset($_FILES[$name]))) {
+                            $errors[] = ["name" => $name, "type" => "required"];
+                        } else {
+                            $value = $data[$name];
+                            $field->value = $value; //Require needs to be the first rule or this line could break something!
+                        }
+                    } else if (isset($data[$name]) && (!empty($data[$name]) || $data[$name] == "0")) {
+                        $value = $data[$name];
+
+                        if ($type == "length") {
+                            $len = strlen($value);
+                            if ($len < $rule["min"] || $len > $rule["max"]) {
+                                $errors[] = ["name" => $name, "type" => "length", "info" => [$rule["min"], $rule["max"]]];
+                            }
+                        } else if ($type == "filter") {
+                            if (!filter_var($value, $rule["filter"])) {
+                                $errors[] = ["name" => $name, "type" => "filter"];
+                            }
+                        } else if ($type == "unique") {
+                            if (isset($rule["ignoreField"])) {
+                                if (!db()->checkIfUnique($rule["table"], $rule["field"], $value, $rule["ignoreField"], $rule["ignoreValue"])) {
+                                    $errors[] = ["name" => $name, "type" => "unique"];
+                                }
+                            } else {
+                                if (!db()->checkIfUnique($rule["table"], $rule["field"], $value)) {
+                                    $errors[] = ["name" => $name, "type" => "unique"];
+                                }
+                            }
+                        } else if ($type == "exist") {
+                            if (!db()->checkIfExists($rule["table"], $rule["field"], $value)) {
+                                $errors[] = ["name" => $name, "type" => "exist"];
+                            }
+                        } else if ($type == "regex") {
+                            $tmp_value = $value;
+                            foreach ($rule["exceptions"] as $exception) {
+                                $tmp_value = str_replace($exception, "", $tmp_value);
+                            }
+                            if (!preg_replace($rule["expression"], "", $tmp_value) != $tmp_value) {
+                                $errors[] = ["name" => $name, "type" => "regex"];
+                            }
+                        } else if ($type == "integer") {
+                            if (!filter_var($value, FILTER_VALIDATE_INT)) {
+                                $errors[] = ["name" => $name, "type" => "integer"];
+                            }
+                            $value = intval($value);
+                        } else if ($type == "range") {
+                            if ($value < $rule["min"] || $value > $rule["max"]) {
+                                $errors[] = ["name" => $name, "type" => "range", "info" => [$rule["min"], $rule["max"]]];
+                            }
+                        } else if ($type == "date") {
+                            if (strtotime($value) == false) {
+                                $errors[] = ["name" => $name, "type" => "date"];
+                            }
+                        } else if ($type == "file") {
+                            if (isset($_FILES[$name])) {
+                                $file = $_FILES[$name];
+                                if ($file["size"] > $rule["maxSize"]) {
+                                    $errors[] = ["name" => $name, "type" => "file_to_big"];
+                                }
+
+                                $extension = strtolower(pathinfo($file["name"], PATHINFO_EXTENSION));
+
+                                if(isset($rule["types"]) && !empty($rule["types"]) && !in_array($extension, $rule["types"])) {
+                                    $errors[] = ["name" => $name, "type" => "file_type"];
+                                }
+                            } else {
+                                $errors[] = ["name" => $name, "type" => "file"];
+                            }
+                        } else {
+                            $errors[] = ["name" => $name, "type" => "contact_admin"]; //Unknown type
+                        }
+
+                        $field->value = $value;
+                    } else {
+                        $field->value = null;
+                    }
+                }
+
+            }
+
+            $formResult->errors = $errors;
+            $formResult->hasErrors = count($errors) > 0;
+            return $formResult;
+        }
+
+
+    }
+
+?>
---
+++ src/Form/Validation/CanValidateMultiForm.php
@@ -0,0 +1,47 @@
+<?php
+
+    namespace ZubZet\Framework\Form\Validation;
+
+    use ZubZet\Framework\Form\Validation\Result;
+
+    trait CanValidateMultiForm {
+
+        use CanValidateForm;
+
+        /**
+         * Validates a "Create Edit Delete" input
+         * @param string $name Name of the input field
+         * @param array $rules Array of rules for validating
+         * @return Result Result of the validation. Needed to perform response actions
+         */
+        public function validateCED($name, $rules) {
+            $errors = [];
+
+            $result = new Result();
+
+            if(isset($_POST[$name])) {
+                $array = $_POST[$name];
+                foreach ($array as $i => $subform) {
+                    $subResult = $this->validateForm($rules, $subform);
+                    $subErrors = $subResult->errors;
+                    foreach ($subErrors as $subError) {
+                        $subError["subname"] = $subError["name"];
+                        $subError["name"] = $name;
+                        $subError["index"] = $i;
+                        $errors[] = $subError;
+                    }
+                }
+            } else {
+                $result->doNothing = true;
+            }
+
+            $result->fields = $rules;
+            $result->hasErrors = count($errors) > 0;
+            $result->errors = $errors;
+            $result->name = $name;
+            return $result;
+        }
+
+    }
+
+?>
