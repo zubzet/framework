2d9bc67070dd721641e9496f57527d979662a498
zierhut.alex <alex@zierhut-it.de>
2026-04-17 23:01:55 +0200

    feat: Add asset proxy for all included webroot assets
    Signed-off-by: zierhut.alex <alex@zierhut-it.de>

--- src/Message/RequestResponseHandler.php
+++ src/Message/RequestResponseHandler.php
@@ -4,6 +4,7 @@
 
     use ZubZet\Framework\ZubZet;
     use ZubZet\Framework\Core\CanRetrieveModel;
+    use ZubZet\Framework\Core\CanRetrieveBooterSettings;
 
     /**
      * Base class for the response and request objects
@@ -11,6 +12,7 @@
     class RequestResponseHandler {
 
         use CanRetrieveModel;
+        use CanRetrieveBooterSettings;
 
         /**
          * @var ZubZet $booter The framework object
@@ -43,24 +45,6 @@
          */
         public function getZRoot() {
             return $this->booter->z_framework_root; 
-        }
-
-        /**
-         * Gets a booter setting
-         * @param string $key Key of the setting
-         * @return mixed Value of the key
-         */
-        public function getBooterSettings($key = null, $useDefault = true, $default = null) {
-            // Return all settings if no key is provided
-            if(empty($key)) return zubzet()->getAllAttributes();
-
-            // Return specific setting if it exists
-            if(isset(zubzet()->{$key})) return zubzet()->{$key};
-
-            // Return default if enabled
-            if($useDefault) return $default;
-
-            throw new \InvalidArgumentException("The setting '$key' does not exist!");
         }
     }
 ?>
