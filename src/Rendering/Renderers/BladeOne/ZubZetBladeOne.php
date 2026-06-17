a89b14b2f95025bd2a92a01349b586d920438145
ulrich.noah <noah.ulrich@zierhut-it.de>
2026-06-17 23:38:48 +0200

    Overwrite baseUrl

--- src/Rendering/Renderers/BladeOne/ZubZetBladeOne.php
+++ src/Rendering/Renderers/BladeOne/ZubZetBladeOne.php
@@ -4,6 +4,14 @@
 
     use eftec\bladeone\BladeOne;
 
-    class ZubZetBladeOne extends BladeOne {}
+    class ZubZetBladeOne extends BladeOne {
+
+        public function __construct($templatePath = null, $compiledPath = null, $mode = 0, $commentMode = 0) {
+            parent::__construct($templatePath, $compiledPath, $mode, $commentMode);
+
+            $this->baseUrl = zubzet()->root;
+        }
+
+    }
 
 ?>
