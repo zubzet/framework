5e8bade92c476d7fe6c73eef167a54ae16eea659
ulrich.noah <noah.ulrich@zierhut-it.de>
2026-09-02 14:03:13 +0200

    test(e2e): cover explicit catalogue domains and the user's preferred language

--- tests/e2e/app/Controllers/TranslationController.php
+++ tests/e2e/app/Controllers/TranslationController.php
@@ -7,6 +7,14 @@
             echo __("language.default");
         }
 
+        public function action_domain(Request $req, Response $res) {
+            echo __("language.domain", domain: "test");
+        }
+
+        public function action_preferred(Request $req, Response $res) {
+            echo __("language.preferred");
+        }
+
     }
 
 ?>
--- tests/e2e/tests/cypress/e2e/translation/locale.cy.js
+++ tests/e2e/tests/cypress/e2e/translation/locale.cy.js
@@ -8,11 +8,22 @@ describe('Translation - Locale Resolution', () => {
         cy.loginAs('locale_en');
         cy.visit('/translation/default-domain');
         cy.contains("Default");
+    });
 
+    it("should use the specified domain when a domain is specified", () => {
+        cy.loginAs('locale_en');
+        cy.visit('/translation/domain');
+        cy.contains("Domain");
+    });
+
+    it("should use the user's preferred language", () => {
         cy.loginAs('locale_de');
-        cy.visit('/translation/default-domain');
-        cy.contains("Standard");
+        cy.visit('/translation/preferred');
+        cy.contains("Bevorzugt");
 
+        cy.loginAs('locale_en');
+        cy.visit('/translation/preferred');
+        cy.contains("Preferred");
     });
 
 });
